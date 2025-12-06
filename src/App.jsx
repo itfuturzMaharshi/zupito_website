cb227dc4619b3d95416c5074bf27c9333dea077f
Maharshi patel <maharship0792@gmail.com>
2025-12-06 12:57:15 +0530

    set 1 option

--- src/App.jsx
+++ src/App.jsx
@@ -41,14 +41,14 @@ function App() {
     // </Router>
 
     <>
-      {/* <Banner />
+      <Banner />
       <DummyHeader />
       <DummyHero />
-      <DummyAction />  */}
+      <DummyAction /> 
 
-      <Banner2 />
+      {/* <Banner2 />
       <Dummy2Header />
-      <Dummy2Hero />
+      <Dummy2Hero /> */}
 
       {/* <Banner3 />
       <Dummy3Header />
