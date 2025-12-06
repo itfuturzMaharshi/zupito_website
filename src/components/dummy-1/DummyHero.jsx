ccbb164fc55dcb67233c28c31d0dbd1781afe475
Maharshi patel <maharship0792@gmail.com>
2025-12-06 18:49:45 +0530

    set ui in mobile screen

--- src/components/dummy-1/DummyHero.jsx
+++ src/components/dummy-1/DummyHero.jsx
@@ -40,7 +40,7 @@ const DummyHero = () => {
 
   return (
     <section className="dummy-1-hero relative w-full bg-white">
-      <div className="w-full px-4 sm:px-6 md:px-12 lg:px-[200px] pt-8 md:pt-12 lg:pt-0 pb-8 md:pb-12 lg:pb-16">
+      <div className="w-full px-4 sm:px-6 md:px-12 lg:px-[180px] pt-8 md:pt-12 lg:pt-0 pb-8 md:pb-12 lg:pb-16">
         <div className="w-full max-w-full mx-auto">
           {/* Navigation Container */}
           <div
